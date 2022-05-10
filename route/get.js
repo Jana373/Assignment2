fbd81516970533e5df7dfc0278832bc7026d43c8
Jana El Mourad <janaselmourad@gmail.com>
2022-05-10 12:02:10 +0300

    more  queries

--- route/get.js
+++ route/get.js
@@ -9,7 +9,7 @@ const Product = require('../models/Product')
 const { default: mongoose } = require('mongoose')
 const Consultant = require('../models/Consultant.js')
 
-// get all the product of an MSME by name name 
+// get all the product of an MSME of name name 
 router.get('/products/:name', async (req, res) => {
 
     try {
@@ -37,7 +37,7 @@ router.get('/consultants/:name', async (req, res) => {
         let msme = await MSME.find({ Name: req.params.name })
         let msme_id = msme[0]["_id"]
 
-        // MSME_id is an index
+        // find consultants info 
         let consultant = await inAction.aggregate([
             { $match: { MSME_id: mongoose.Types.ObjectId(msme_id) } },
             {
@@ -53,14 +53,14 @@ router.get('/consultants/:name', async (req, res) => {
                     "_id": 0,
                     "First_Name": "$consultantinfo.First_Name",
                     "Industry": "$consultantinfo.Industry"
-                   
+
                 }
             }
 
 
         ])
 
-        //let consultant = await inAction.find({ MSME_id: msme_id }, { 'First_Name': 1, 'Experience': 1, '_id': 0 })
+
         res.json(consultant)
 
     }
@@ -70,7 +70,64 @@ router.get('/consultants/:name', async (req, res) => {
 
 
 })
+// sum coaching hours of an msme of name name 
 
+router.get('/hours/:name', async (req, res) => {
+
+    try {
+
+        // get the id of the consultant
+        let msme = await MSME.find({ Name: req.params.name })
+        let msme_id = msme[0]["_id"]
+
+
+        let consultant = await inAction.aggregate([
+            { $match: { MSME_id: mongoose.Types.ObjectId(msme_id) } },
+            {
+                $group: {
+                    // summ all matched elements
+                    "_id": 0,
+                    "Total Coaching Hours": { $sum: "$coaching_hours" }
+                }
+            }
+
+
+        ])
+
+
+
+        res.json(consultant)
+
+
+    }
+    catch (error) {
+        res.json(error)
+    }
+
+
+})
+// get expected coaching hours of an msme
+router.get('/expected/:name', async (req, res) => {
+    try {
+        let msme = await MSME.find({ Name: req.params.name })
+        let msme_id = msme[0]["_id"]
+
+        let expected = await PreConsulting.aggregate([
+            { $match: { MSME_id: mongoose.Types.ObjectId(msme_id) } },
+            {
+                $project: {
+                    "_id": 0,
+                    "Expected Coaching Hours": "$Expected_coaching_hours"
+                }
+            }
+        ])
+        res.json(expected)
+    }
+
+    catch (error) {
+        res.json(error)
+    }
+})
 
 
 
